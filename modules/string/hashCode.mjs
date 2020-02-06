9059ef80a4b8b62288ec7112e9ba4826476b1123
ic <ic.czech+github@gmail.com>
2020-02-06 17:09:43 +0100

    convert number of base 10 to base 36
    … and set return type

--- modules/string/hashCode.mjs
+++ modules/string/hashCode.mjs
@@ -9,7 +9,8 @@ export class append
 		if ( typeof s()[ FUNCTION_NAME ] !== 'function' ) {
 
 			Object.defineProperty( s.prototype, FUNCTION_NAME, {
-				value: function () : String
+				/** @returns {String} */
+				value: function ()
 				{
 					let hash = 0;
 					for ( let i = 0; i < this.length; i++ ) {
@@ -17,7 +18,7 @@ export class append
 						hash = ( ( hash << 5 ) - hash ) + character;
 						hash = hash & hash; // Convert to 32bit integer
 					}
-					return Number(hash).toString(36);
+					return Number( hash ).toString( 36 );
 				},
 				writable: false,
 				configurable: false,
