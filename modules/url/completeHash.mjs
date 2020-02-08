d620ee0859cc57a85e5593f4feea4a8d3483da58
ic <ic.czech+github@gmail.com>
2020-02-08 13:35:22 +0100

    for hashing url

---
+++ modules/url/completeHash.mjs
@@ -0,0 +1,25 @@
+'use strict';
+
+export class append
+{
+	constructor ( u = URL )
+	{
+		Object.defineProperty( u.prototype, 'completeHash', {
+			/** @returns {String} */
+			value: function ()
+			{
+				let hash = 0;
+				const link = String( this.href );
+				for ( let i = 0; i < link.length; i++ ) {
+					const character = link.charCodeAt( i );
+					hash = ( ( hash << 5 ) - hash ) + character;
+					hash = hash & hash; // Convert to 32bit integer
+				}
+				return Number( hash ).toString( 36 );
+			},
+			writable: false,
+			configurable: false,
+			enumerable: false,
+		} );
+	}
+}
