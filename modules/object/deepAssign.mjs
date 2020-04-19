7158311def9e0f1862e79acea316c6aa2fd053ad
Michal Lupečka <ic.czech@gmail.com>
2020-04-19 19:34:23 +0200

    removed 'use strict'; and added deepAssign for Object

---
+++ modules/object/deepAssign.mjs
@@ -0,0 +1,30 @@
+export class append
+{
+	constructor ( /** @type {Object} */ o = Object )
+	{
+		const FUNCTION_NAME = 'deepAssign';
+
+		if ( typeof o()[ FUNCTION_NAME ] !== 'function' ) {
+			o.defineProperty( o.prototype, FUNCTION_NAME, {
+				value: function ( /** @type {Array} */ ...args )
+				{
+					let currentLevel = o;
+					args.forEach( ( /** @type {Object} */ source ) =>
+					{
+						o.entries( source ).forEach( ( [ /** @type {String} */ key, value ] ) =>
+						{
+							if ( value instanceof o && key in currentLevel ) {
+								value = o[ FUNCTION_NAME ]( currentLevel[ key ], value );
+							}
+							currentLevel = { ...currentLevel, [ key ]: value };
+						} );
+					} );
+					return currentLevel;
+				},
+				writable: false,
+				configurable: false,
+				enumerable: false,
+			} );
+		}
+	}
+}
