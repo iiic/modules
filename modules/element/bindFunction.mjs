f97b0fd2772a42be522f16f0cd68278644cf2327
Michal Lupečka <ic.czech@gmail.com>
2020-06-12 10:58:42 +0200

    new element bindFunction()

---
+++ modules/element/bindFunction.mjs
@@ -0,0 +1,19 @@
+export class append
+{
+	constructor ( e = Element )
+	{
+		const FUNCTION_NAME = 'bindFunction';
+
+		if ( typeof e[ FUNCTION_NAME ] !== 'function' ) {
+			Object.defineProperty( e.prototype, FUNCTION_NAME, {
+				value: function ( /** @type {String} */ name, /** @type {Function} */ newFunction )
+				{
+					this[ name ] = newFunction.bind( this );
+				},
+				writable: false,
+				configurable: false,
+				enumerable: false,
+			} );
+		}
+	}
+}
