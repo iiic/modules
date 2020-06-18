ea3dafd5a03fa99c39728d08042a312b3db6e10e
Michal Lupečka <ic.czech@gmail.com>
2020-06-18 22:18:46 +0200

    bug repaired

--- modules/string/splitIntoWords.mjs
+++ modules/string/splitIntoWords.mjs
@@ -8,13 +8,15 @@ export class append
 			Object.defineProperty( s.prototype, FUNCTION_NAME, {
 				value: function ( /** @type {String} */ chars )
 				{
-					const TEMP_SEPARATOR = '{this is temporarily separator string}';
+					const TEMP_SEPARATOR = '{this is separator string used as temporarily replacement}';
 
 					const regexpChars = chars ? chars : '  	.,;?!…:„“–+-';
 
 					const regexp = new RegExp( '([' + regexpChars + ']+)', 'g' );
 
-					return this.replace( regexp, TEMP_SEPARATOR ).split( TEMP_SEPARATOR );
+					const words = this.replace( regexp, TEMP_SEPARATOR ).split( TEMP_SEPARATOR );
+
+					return words.filter( Boolean );
 				},
 				writable: false,
 				configurable: false,
